b3006c4642631e86c7af56858d56911d081799f4
Julio Lozano <Julyodavyd@gmail.com>
2025-04-19 13:02:22 -0500

    Modified ComentariosController

--- BibliotecaAPI/Controllers/ComentariosController.cs
+++ BibliotecaAPI/Controllers/ComentariosController.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using BibliotecaAPI.Datos;
 using BibliotecaAPI.DTOs;
+using BibliotecaAPI.Entidades;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -33,6 +35,98 @@ namespace BibliotecaAPI.Controllers
                 .ToListAsync();
 
             return mapper.Map<List<ComentarioDTO>>(comentarios);
+        }
+
+        [HttpGet("{id}",Name ="ObtenerComentario")]
+        public async Task<ActionResult<ComentarioDTO>> Get (Guid id)
+        {
+            var comentario = await context.Comentarios.FirstOrDefaultAsync(x => x.Id == id);
+            if (comentario is null)
+            {
+                return NotFound();
+            }
+            return mapper.Map<ComentarioDTO>(comentario);
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult>Post (int libroId, ComentarioCreacionDTO comentarioCreacionDTO) 
+        {
+            var existeLibro = await context.Libros.AnyAsync(x => x.Id == libroId);
+            if (!existeLibro)
+            {
+                return NotFound();
+            }
+            var comentario = mapper.Map<Comentario>(comentarioCreacionDTO);
+
+            comentario.LibroId = libroId;
+            comentario.FechaPublicacion = DateTime.UtcNow;
+
+            context.Add(comentario);
+
+            await context.SaveChangesAsync();
+
+            var comentarioDTO = mapper.Map<ComentarioDTO>(comentario);
+
+            return CreatedAtRoute("ObtenerComentario", new { id = comentario.Id, libroId }, comentarioDTO);
+
+        }
+
+        [HttpPatch("{id}")]
+        public async Task<ActionResult> Patch(Guid id, int libroId, JsonPatchDocument<ComentarioPatchDTO> patchDoc)
+        {
+            if (patchDoc is null)
+            {
+                return BadRequest();
+            }
+
+            var existeLibro = await context.Libros.AnyAsync(x => x.Id == libroId);
+            if (!existeLibro)
+            {
+                return NotFound();
+            }
+            
+
+            var comentarioDB = await context.Comentarios.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (comentarioDB is null)
+            {
+                return NotFound();
+
+            }
+
+            var comentarioPatchDTO = mapper.Map<ComentarioPatchDTO>(comentarioDB);
+
+            patchDoc.ApplyTo(comentarioPatchDTO, ModelState);
+
+            var esValido = TryValidateModel(comentarioPatchDTO);
+            if (!esValido)
+            {
+                return ValidationProblem();
+            }
+            mapper.Map(comentarioPatchDTO, comentarioDB);
+            await context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(Guid id,int libroId)
+        {
+            var existeLibro = await context.Libros.AnyAsync(x => x.Id == libroId);
+            if (!existeLibro)
+            {
+                return NotFound();
+            }
+            var registrosBorrados = await context.Comentarios.Where(x => x.Id == id).ExecuteDeleteAsync();
+
+            if (registrosBorrados==0)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+
         }
     }
 }
